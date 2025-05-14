e6cc338ffaa01eb288e61957de7997d4a8f733d0
Dinis <dinisfreiredesousa@gmail.com>
2025-05-14 23:02:36 +0100

    segundo commit

--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,13 @@
-const connection = require('./con');
+const con = require('./database');
+const express = require('express');
+const app = express()
+
+let HOST = process.env.HOST || 'localhost';
+let PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, HOST, (error) => {
+    console.log(`YOUR SERVER IS RUNNING AT http://${HOST}:${PORT}`)
+})
+
+
