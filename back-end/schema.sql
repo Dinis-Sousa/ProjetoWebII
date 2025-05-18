d70708cad79f21bc586914e2b59cae7de99aa206
Dinis <dinisfreiredesousa@gmail.com>
2025-05-18 20:06:12 +0100

    atualização do schema

--- back-end/schema.sql
+++ back-end/schema.sql
@@ -1,16 +1,33 @@
 CREATE DATABASE projetoii_db;
 USE projetoii_db;
 
+CREATE TABLE Escola (
+    escola_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    morada VARCHAR(255) NOT NULL,
+    codigoPostal VARCHAR(255) NOT NULL,
+    localidade VARCHAR(255) NOT NULL,
+    telefone VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    nivelCertificacao ENUM('BÁSICO', 'MÉDIO', 'AVANÇADO') NOT NULL
+);
+
+CREATE TABLE AreaTematica (
+    area_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    descriacao TEXT NOT NULL
+);
+
 CREATE TABLE Utilizador (
     user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     escola_id INT,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    passwordHssh VARCHAR(255) NOT NULL,
+    passwordHash VARCHAR(255) NOT NULL,
     perfil ENUM('ADMIN', 'ALUNO', 'COLABORADOR'),
     dataRegisto DATETIME DEFAULT CURRENT_TIMESTAMP,
-    pontos INT NOT NULL, 
-    FOREIGN KEY (escola_id) REFERENCES escola(escola_id)
+    pontos INT NOT NULL,
+    FOREIGN KEY (escola_id) REFERENCES Escola(escola_id)
 );
 
 CREATE TABLE Atividade (
@@ -21,19 +38,14 @@ CREATE TABLE Atividade (
     dataInicio DATE NOT NULL,
     dataFim DATE NOT NULL,
     estado ENUM('CONCLUIDA', 'EM PROGRESSO', 'PENDENTE'),
-    FOREIGN KEY (area_id) REFERENCES AreaTematics(area_id)
+    FOREIGN KEY (area_id) REFERENCES AreaTematica(area_id)
 );
 
-CREATE TABLE AreaTematics (
-    area_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
-    descriacao TEXT NOT NULL
-);
 CREATE TABLE Sessao (
     sessao_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     atividade_id INT,
-    dataMarcada Date NOT NULL,
-    horaMarcada TIMESTAMP NOT NULL,
+    dataMarcada DATE NOT NULL,
+    horaMarcada TIME NOT NULL,
     vagas INT NOT NULL,
     FOREIGN KEY (atividade_id) REFERENCES Atividade(atividade_id)
 );
@@ -42,25 +54,18 @@ CREATE TABLE InscricaoVoluntariado (
     sessao_id INT NOT NULL,
     user_id INT NOT NULL,
     presença BOOLEAN NOT NULL,
-    PRIMARY KEY (sessao_id, user_id)
-);
-
-CREATE TABLE Escola (
-    escola_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
-    morada VARCHAR(255) NOT NULL,
-    codigoPostal VARCHAR(255) NOT NULL,
-    localidade VARCHAR(255) NOT NULL,
-    telefone VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    nivelCertificacao enum('BÁSICO', 'MÉDIO', 'AVANÇADO') NOT NULL
+    PRIMARY KEY (sessao_id, user_id),
+    FOREIGN KEY (sessao_id) REFERENCES Sessao(sessao_id),
+    FOREIGN KEY (user_id) REFERENCES Utilizador(user_id)
 );
 
 CREATE TABLE AdesaoAtividade (
     atividade_id INT NOT NULL,
     escola_id INT NOT NULL,
     aderiu BOOLEAN NOT NULL,
-    PRIMARY KEY (atividade_id, escola_id)
+    PRIMARY KEY (atividade_id, escola_id),
+    FOREIGN KEY (atividade_id) REFERENCES Atividade(atividade_id),
+    FOREIGN KEY (escola_id) REFERENCES Escola(escola_id)
 );
 
 CREATE TABLE Conquistas (
@@ -74,5 +79,7 @@ CREATE TABLE ConUser (
     user_id INT NOT NULL,
     conquista_id INT NOT NULL,
     estado BOOLEAN NOT NULL,
-    PRIMARY KEY (user_id, conquista_id)
+    PRIMARY KEY (user_id, conquista_id),
+    FOREIGN KEY (user_id) REFERENCES Utilizador(user_id),
+    FOREIGN KEY (conquista_id) REFERENCES Conquistas(conquista_id)
 );
