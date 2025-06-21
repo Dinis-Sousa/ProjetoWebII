2d8003115563a08d169497a06c606e1b6566f25a
Dinis <dinisfreiredesousa@gmail.com>
2025-06-21 12:27:21 +0100

    remover Cloudinary

--- back-end/routes/cloud.routes.js
+++
@@ -1,8 +0,0 @@
-const upload = require('../utils/multer.js');
-const express = require('express');
-const router = express.Router()
-
-const CloudControllers = require('../controllers/CloudinaryControllers.js');
-
-router.post('/upload', CloudControllers.uploadImg);
-
--- back-end/utils/multer.js
+++
@@ -1,9 +0,0 @@
-// middleware/multer.js
-import multer from 'multer';
-import { memoryStorage } from 'multer';
-
-const storage = memoryStorage();
-
-const upload = multer({ storage });
-
-export default upload;
